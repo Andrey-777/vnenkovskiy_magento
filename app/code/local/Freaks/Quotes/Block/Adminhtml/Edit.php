e2afd18872ac7e9ef6ea65d2aefefba08d3b4eab
avnenkovskyi <avnenkovskyi@malkosua.com>
2013-11-21 11:23:16 +0200

    11:22 11/21/2013 create CRUD modul on backend

---
+++ app/code/local/Freaks/Quotes/Block/Adminhtml/Edit.php
@@ -0,0 +1,24 @@
+<?php
+class Freaks_Quotes_Block_Adminhtml_Edit extends Mage_Adminhtml_Block_Widget_Form_Container
+{
+    protected function _construct()
+    {
+        $this->_blockGroup = 'freaks_quotes';
+        $this->_mode = 'edit';
+        $this->_controller = 'adminhtml';
+        
+        $quote_id = (int)$this->getRequest()->getParam($this->_objectId);
+        $quote = Mage::getModel('freaks_quotes/quote')->load($quote_id);
+        Mage::register('current_quote', $quote);
+    }
+ 
+    public function getHeaderText()
+    {
+        $quote = Mage::registry('current_quote');
+        if ($quote->getId()) { 
+            return Mage::helper('freaks_quotes')->__("Edit Quote '%s'", $this->escapeHtml($quote->getName()));
+        } else {
+            return Mage::helper('freaks_quotes')->__("Add new Quote");
+        }
+    }
+}
