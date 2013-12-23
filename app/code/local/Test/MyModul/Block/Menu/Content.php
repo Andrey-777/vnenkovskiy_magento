b0aa10e57f4a1b78c72811803dda0c4dbeedff50
avnenkovskyi <avnenkovskyi@malkosua.com>
2013-12-23 11:17:56 +0200

    edit page.xml file

--- app/code/local/Test/MyModul/Block/Menu/Content.php
+++ app/code/local/Test/MyModul/Block/Menu/Content.php
@@ -1,5 +1,8 @@
 <?php
-/* @todo Try to delete this block */
 class Test_MyModul_Block_Menu_content extends Mage_Core_Block_Template
-{   
+{
+    protected function getServiceHelper()
+    {
+        return Mage::helper('test_paginator');
+    }    
 }
